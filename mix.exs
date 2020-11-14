d879752f3ae00a3a632470968d131b5af3e3e232
Rod McLaughlin <rodmclaughlin@gmail.com>
2020-11-14 10:33:46 +0300

    [add-tiny-mce-local]: Removed refs to ecto from mix.exs, and added new css file to edit page, to make font bigger - not done yet

--- mix.exs
+++ mix.exs
@@ -41,9 +41,9 @@ defmodule SimpleMongoApp.Mixfile do
 
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "test"]
     ]
   end
 end
